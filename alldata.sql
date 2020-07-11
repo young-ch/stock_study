94d575b3ec5d93aa03e629d0a719da06175af93f
young <godsky1990@gmail.com>
2020-07-11 23:17:06 +0900

    all

--- alldata.sql
+++ alldata.sql
@@ -1,4 +1,3 @@
-Usage: mysqldump [OPTIONS] database [tables]
-OR     mysqldump [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
-OR     mysqldump [OPTIONS] --all-databases [OPTIONS]
-For more options, use mysqldump --help
+version https://git-lfs.github.com/spec/v1
+oid sha256:114ca562d18726d2475851e633e1f2b4b854c38f9e903324df14c62d8e08ddc1
+size 207
